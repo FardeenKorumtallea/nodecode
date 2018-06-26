c342f1834d1b4e9e8a1d092d04be17697856935b
FardeenKorumtallea <40594328+FardeenKorumtallea@users.noreply.github.com>
2018-06-26 11:20:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var app = express();
 app.use(express.static('public'));
 console.log('Exact name: ' + process.env.ORA_INSTANCE_NAME);
 runGetRequest();
-//runDatabaseQuery();
+runDatabaseQuery();
 
 // Does a GET request to ip.jsontest.com
 function runGetRequest() {
     
     //sample URL.
-    var url = "http://140.86.15.104:3000/fighters/45/6/green/FardeenKorumtallea";
+    var url = "http://140.86.15.104:3000/fighters/33/6/green/FardeenKorumtallea";
     request(url, function(error, response, body) {
         if(!error) {
             console.log(body);
@@ -26,7 +26,7 @@ function runGetRequest() {
 
 //Executes a SQL query
 function runDatabaseQuery() {
-    connection.query("SELECT * FROM SampleTable", function(error, rows, fields) {
+    connection.query("SELECT * FROM SecretTable", function(error, rows, fields) {
         if(!error) {
             console.log(rows);
         } else {
