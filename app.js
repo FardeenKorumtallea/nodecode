93fce7f898197157126c5c823044c5af3c7f7f56
FardeenKorumtallea <40594328+FardeenKorumtallea@users.noreply.github.com>
2018-06-26 11:12:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ runGetRequest();
 function runGetRequest() {
     
     //sample URL.
-    var url = "http://140.86.15.104:3000/fighters/45/9/green/FardeenKorumtallea";
+    var url = "http://140.86.15.104:3000/fighters/45/3/green/FardeenKorumtallea";
     request(url, function(error, response, body) {
         if(!error) {
             console.log(body);
